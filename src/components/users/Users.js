5e1968097f4f93a9dd81da4c9716a574076b1d0b
believemaster <believemasters@gmail.com>
2020-01-24 20:41:14 +0530

    Http requests updating state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,39 +2,10 @@ import React, { Component } from "react";
 import UserItem from "./UserItem";
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: 136,
-        login: "simonjefford",
-        avatar_url: "https://avatars2.githubusercontent.com/u/136?v=4",
-        html_url: "https://github.com/simonjefford"
-      },
-      {
-        id: 137,
-        login: "josh",
-        avatar_url: "https://avatars2.githubusercontent.com/u/137?v=4",
-        html_url: "https://github.com/josh"
-      },
-      {
-        id: 138,
-        login: "stevedekorte",
-        avatar_url: "https://avatars3.githubusercontent.com/u/138?v=4",
-        html_url: "https://github.com/stevedekorte"
-      },
-      {
-        id: 139,
-        login: "leahneukirchen",
-        avatar_url: "https://avatars3.githubusercontent.com/u/139?v=4",
-        html_url: "https://github.com/leahneukirchen"
-      }
-    ]
-  };
-
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
